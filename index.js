720269795f23f91a772c8ae1b0ae8dc67083ee8b
randa yahya <randaalrayan96@gmial.com>
2020-05-15 10:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 let main = document.querySelector('main');
 main.remove()
 let newHeader = document.createElement('h1')
+<<<<<<< HEAD
 newHeader.id="victory";
 newHeader.innerHTML = " randa is the champion";
+=======
+newHeader.id=
+
+h1.innerHTML = 'my name is randa';
+>>>>>>> 60e246dfe863e0241786e426a859f90a9a4d468f
