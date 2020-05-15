ecf61e5ce74df3e5de7089baefacff6c7b3a30e9
randa yahya <randaalrayan96@gmial.com>
2020-05-15 10:23:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.querySelector('main');
+main.remove()
+let newHeader = document.createElement('h1')
+newHeader.id="victory";
+newHeader.innerHTML = " randa is the champion";
